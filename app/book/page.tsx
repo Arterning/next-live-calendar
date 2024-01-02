1204361241ab8e563c6f57bb5235bff2f36e4478
Arterning <ning.huang.ridson@gmail.com>
2024-01-02 15:11:42 +0800

    update ui

--- app/book/page.tsx
+++ app/book/page.tsx
@@ -1,8 +1,6 @@
 "use client"
 
 import { Button } from "@/components/ui/button"
-import { Calendar } from "@/components/ui/calendar"
-import toast from "react-hot-toast"
 import { PlusCircle } from "lucide-react"
 
 import { useUser } from "@clerk/nextjs";
@@ -14,22 +12,7 @@ const BookPage = () => {
     const { isLoaded, isSignedIn, user } = useUser();
 
     return (
-      <div className="flex gap-3 justify-start p-4 space-x-2 h-full">
-        <Button
-          onClick={() => {
-            console.log("clicked");
-            toast.success("Hey, You are on Book Page");
-          }}
-        >
-          Button
-        </Button>
-        <Button
-          variant="outline"
-          onClick={() => toast.error("Something Wrong")}
-        >
-          Cancel
-        </Button>
-
+      <div className="flex gap-3 items-center justify-center p-4 space-x-8 h-full">
         {user != null && (
           <Link href="/book/create">
             <Button>
@@ -38,9 +21,9 @@ const BookPage = () => {
             </Button>
           </Link>
         )}
-
-        <span>Book Page</span>
-        <Calendar />
+        <p>
+          Books are important
+        </p>
       </div>
     );
 }
--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,9 +1,26 @@
+"use client"
+
+import { Button } from "@/components/ui/button";
+import { Calendar } from "@/components/ui/calendar";
+import toast from "react-hot-toast";
+
 const TestPage = () => {
-    return (
-        <div>
-            Test Page
-        </div>
-    )
-}
+  return (
+    <div>
+      <Button
+        onClick={() => {
+          console.log("clicked");
+          toast.success("Hey, You are on Book Page");
+        }}
+      >
+        Button
+      </Button>
+      <Button variant="outline" onClick={() => toast.error("Something Wrong")}>
+        Cancel
+      </Button>
+      <Calendar />
+    </div>
+  );
+};
 
 export default TestPage;
