4dd456eec9e6d81c54f8846a6fcafaf98517ba97
Arterning <ning.huang.ridson@gmail.com>
2024-01-03 10:47:15 +0800

    update

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,3 +1,4 @@
+import LinkCard from '@/components/link-card';
 import React from 'react';
 
 interface IDashBoardPageProps {
@@ -7,9 +8,12 @@ interface IDashBoardPageProps {
 const DashBoardPage: React.FC<IDashBoardPageProps> = (props) => {
 
     return (
-      <>
-        DashBoardPage
-      </>
+      <div className='p-6'>
+        <h2>DashBoardPage</h2>
+        <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
+          <LinkCard href="#TODO" title="Docs" desc="Find in-depth information about Next.js features and API."/>
+        </div>
+      </div>
     );
 };
 
--- components/file-base64-upload.tsx
+++ components/file-base64-upload.tsx
@@ -49,7 +49,7 @@ const ImageUpload: React.FC<DropzoneProps> = ({ onChange, label, value, disabled
           />
         </div>
       ) : (
-        <p className="text-black dark:text-white">{label}</p>
+        <p>{label}</p>
       )}
     </div>
    );
