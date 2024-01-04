8ac53620289990fbc23f47cfe12b989058619495
Arterning <ning.huang.ridson@gmail.com>
2024-01-04 09:05:42 +0800

    table

--- app/analytics/page.tsx
+++ app/analytics/page.tsx
@@ -1,16 +1,38 @@
-import React from 'react';
+import React from "react";
+import { DataTable } from "./data-table";
+import { Payment, columns } from "./columns";
 
-interface IAnalyticsProps {
+interface IAnalyticsProps {}
 
-}
+const Analytics: React.FC<IAnalyticsProps> = async (props) => {
+  async function getData(): Promise<Payment[]> {
+    // Fetch data from your API here.
+    return [
+      {
+        id: "728ed52f",
+        amount: 100,
+        status: "pending",
+        email: "m@example.com",
+      },
+      {
+        id: "728ed52r",
+        amount: 180,
+        status: "pending",
+        email: "m@example.com",
+      },
+      // ...
+    ];
+  }
 
-const Analytics: React.FC<IAnalyticsProps> = (props) => {
+  const data = await getData();
 
-    return (
-      <>
-        Analytics
-      </>
-    );
+  return (
+    <>
+      <div className="container mx-auto py-10">
+        <DataTable columns={columns} data={data} />
+      </div>
+    </>
+  );
 };
 
-export default Analytics
+export default Analytics;
--- app/contacts/page.tsx
+++ app/contacts/page.tsx
@@ -1,16 +1,22 @@
-import React from 'react';
+import React from "react";
 
-interface IContactsProps {
-
-}
+interface IContactsProps {}
 
 const Contacts: React.FC<IContactsProps> = (props) => {
-
-    return (
-      <>
-        Contacts
-      </>
-    );
+  return (
+    <>
+      <div>
+        <p>Contacts</p>
+        <div>
+          you know waht
+          <p>
+            <a href="https://nextjs.org">Next.js</a>
+          </p>
+        </div>
+      </div>
+      Contacts
+    </>
+  );
 };
 
-export default Contacts
+export default Contacts;
